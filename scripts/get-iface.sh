84e5f8317abe71ebd267e2cbb54ac080ff105737
Ron Cantrell <ron@cantrelloffice.cloud>
2025-09-23 16:43:16 +0000

    Update get-iface.sh

--- scripts/get-iface.sh
+++ scripts/get-iface.sh
@@ -1,1 +1,5 @@
-export IFACE="$(ip -o -4 route show to default 2>/dev/null | awk '{print $5; exit}')" ; [ -z "$IFACE" ] && export IFACE="$(ip -o -6 route show to default 2>/dev/null | awk '{print $5; exit}')"
+#!/usr/bin/env bash
+set -euo pipefail
+export IFACE=$(ip -o -4 route show to default 2>/dev/null | awk '{print $5; exit}')
+echo "The network interface is: ${IFACE}"
+echo
